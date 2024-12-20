c88c1b710e38cf83c329cefc31a2b0eebed7dd6a
Pj Bonbon <90759923+pjjj23@users.noreply.github.com>
2024-12-20 11:02:53 +0800

    newlyUpdated

--- myproject/urls.py
+++ myproject/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
+    path('', include('docs.urls')),
 ]
 
 # Serve static files without collectstatic when DEBUG is False
